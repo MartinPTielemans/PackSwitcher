711149fd41661b14ecca5460727e310a25b3fe12
Martin P. Tielemans <Martin@tielemans.dev>
2025-06-01 20:31:21 +0200

    fix formatting

--- eslint.config.js
+++ eslint.config.js
@@ -34,10 +34,13 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
     },
   },
-] 
+]
